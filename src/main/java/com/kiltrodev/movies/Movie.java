131510cb2b5ff240507b72f52ef5278df65394db
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-12 16:32:54 -0300

    movie controller

--- src/main/java/com/kiltrodev/movies/Movie.java
+++ src/main/java/com/kiltrodev/movies/Movie.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -16,11 +17,13 @@ import java.util.List;
 public class Movie {
     @Id
     private ObjectId id;
-    private String imbdId;
+    private String imdbId;
     private String title;
     private String releaseDate;
     private String trailerLink;
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewsIds;
 }
---
+++ src/main/java/com/kiltrodev/movies/MovieRepository.java
@@ -0,0 +1,4 @@
+package com.kiltrodev.movies;
+
+public interface MovieRepository {
+}
