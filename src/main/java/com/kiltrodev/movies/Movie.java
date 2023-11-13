d6acaf2206d5d316a678b01ec9a377b4df7ebefd
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-13 11:46:41 -0300

    review done

--- src/main/java/com/kiltrodev/movies/Movie.java
+++ src/main/java/com/kiltrodev/movies/Movie.java
@@ -25,5 +25,5 @@ public class Movie {
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference
-    private List<Review> reviewsIds;
+    private List<Review> reviewIds;
 }
